c6844fff3e650834deed99912c4bbf7bf5749d9e
Tim Wilde <pause@krellis.org>
2011-02-15 17:31:07 +0000

    Import of TWILDE/Parallel-Fork-BossWorker-0.04_01 from CPAN.
    gitpan-cpan-distribution: Parallel-Fork-BossWorker
    gitpan-cpan-version:      0.04_01
    gitpan-cpan-path:         TWILDE/Parallel-Fork-BossWorker-0.04_01.tar.gz
    gitpan-cpan-author:       TWILDE
    gitpan-cpan-maturity:     developer

--- lib/Parallel/Fork/BossWorker.pm
+++ lib/Parallel/Fork/BossWorker.pm
@@ -1,4 +1,7 @@
 package Parallel::Fork::BossWorker;
+#
+# $Id: BossWorker.pm 8 2011-02-15 13:24:52Z twilde $
+#
 
 use 5.008008;
 use strict;
@@ -6,216 +9,231 @@ use warnings;
 use Carp;
 use Data::Dumper qw(Dumper);
 use IO::Handle;
+use IO::Select;
 
 # Perl module variables
 our @ISA = qw();
-our $VERSION = '0.03';
+our $VERSION = '0.04_1';
 
 sub new {
-	my $class = shift;
-	my %values = @_;
-	
-	my $self = {
-		result_handler => $values{result_handler} || undef,  # Method for handling output of the workers
-		worker_count   => $values{worker_count}   || 10,     # Number of workers
-		global_timeout => $values{global_timeout} || 0,      # Number of seconds before the worker terminates the job, 0 for unlimited
-		work_handler   => $values{work_handler},             # Handler which will process the data from the boss
-		work_queue     => [],
-		msg_delimiter  => "\0\0\0"
-	};
-	bless $self, ref($class) || $class;
+    my $class = shift;
+    my %values = @_;
+    
+    my $self = {
+        result_handler => $values{result_handler} || undef,  # Method for handling output of the workers
+        worker_count   => $values{worker_count}   || 10,     # Number of workers
+        global_timeout => $values{global_timeout} || 0,      # Number of seconds before the worker terminates the job, 0 for unlimited
+        work_handler   => $values{work_handler},             # Handler which will process the data from the boss
+        work_queue     => [],
+        msg_delimiter  => "\0\0\0",
+        select         => IO::Select->new(),
+    };
+    bless $self, ref($class) || $class;
 
-	# The work handler is required
-	if (not defined $self->{work_handler}) {
-		croak("Parameters \`work_handler' is required.");
-	}
+    # The work handler is required
+    if (not defined $self->{work_handler}) {
+        croak("Parameters \`work_handler' is required.");
+    }
 
-	return $self;
+    return $self;
 }
 
 sub add_work(\@) {
-	my $self = shift;
-	my $work = shift;
-	push(@{ $self->{work_queue} }, $work);
+    my $self = shift;
+    my $work = shift;
+    unshift (@{ $self->{work_queue} }, $work);
 }
 
 sub process {
-	my $self = shift;
-	my $handler = shift;
-	
-	eval {
-		
-		# If a worker dies, there's a problem
-		local $SIG{CHLD} = sub {
-			my $pid = wait();
-			if (defined $self->{workers}->{$pid}) {
-				confess("Worker $pid died.");
-			}
-		};
-		
-		# Start the workers
-		$self->start();
-		
-		# Handy pipe reference
-		my $from_workers = $self->{from_workers};
-		
-		# Read from the workers, loop until they all shut down
-		while(my $result = $self->receive($from_workers)) {
-			
-			# Process the result handler
-			if ($result->{data} && defined $self->{result_handler}) {
-				&{ $self->{result_handler} }( $result->{data} );
-			}
-			
-			# If there's still work to be done, send it to the worker, otherwise shut it down
-			if ($#{ $self->{work_queue} } > -1) {
-				my $worker = $self->{workers}->{$result->{pid}};
-				$self->send(
-					$self->{workers}->{ $result->{pid} }, # Worker's pipe
-					pop(@{ $self->{work_queue} })
-				);
-			} else {
-				my $fh = $self->{workers}->{ $result->{pid} };
-				delete($self->{workers}->{ $result->{pid} });
-				close($fh);
-			}
-		}
-	};
-	
-	if ($@) {
-		croak($@);
-	}
+    my $self = shift;
+    my $handler = shift;
+    
+    eval {
+        
+        # If a worker dies, there's a problem
+        local $SIG{CHLD} = sub {
+            my $pid = wait();
+            if (defined $self->{workers}->{$pid}) {
+                confess("Worker $pid died.");
+            }
+        };
+        
+        # Start the workers
+        $self->start();
+        
+        # Read from the workers, loop until they all shut down
+        while (%{$self->{workers}}) {
+            while (my @ready = $self->{select}->can_read()) {
+                foreach my $fh (@ready) {
+                    my $result = $self->receive($fh);
+                    if (!$result) {
+                        $self->{select}->remove($fh);
+                        print STDERR "$fh got eof\n";
+                        next;
+                    }
+                    
+                    # Process the result handler
+                    if ($result->{data} && defined $self->{result_handler}) {
+                        &{ $self->{result_handler} }( $result->{data} );
+                    }
+                    
+                    # If there's still work to be done, send it to the worker, otherwise shut it down
+                    if ($#{ $self->{work_queue} } > -1) {
+                        my $worker = $self->{workers}->{$result->{pid}};
+                        $self->send(
+                            $self->{workers}->{ $result->{pid} }, # Worker's pipe
+                            pop(@{ $self->{work_queue} })
+                        );
+                    } else {
+                        $self->{select}->remove($fh);
+                        my $fh = $self->{workers}->{ $result->{pid} };
+                        delete($self->{workers}->{ $result->{pid} });
+                        close($fh);
+                    }
+                }
+            }
+        }
+        
+        # Wait for our children so the process table won't fill up
+        while ((my $pid = wait()) != -1) { }
+    };
+    
+    if ($@) {
+        croak($@);
+    }
 }
 
 sub start {
-	my $self = shift();
-	
-	# Create a pipe for the workers to communicate to the boss
-	pipe($self->{from_workers}, $self->{to_boss});
-	
-	# Create the workers
-	foreach (1..$self->{worker_count}) {
-		
-		# Open a pipe for the worker
-		pipe(my $from_boss, my $to_worker);
-		
-		# Fork off a worker
-		my $pid = fork();
-		
-		if ($pid > 0) {
-			
-			# Boss
-			$self->{workers}->{$pid} = $to_worker;
-			
-		} elsif ($pid == 0) {
-			
-			# Worker
-			
-			# Close unused pipes
-			close($self->{from_workers});
-			close($to_worker);
-			open(STDIN, '/dev/null');
-			
-			# Setup communication pipes
-			my $to_boss = $self->{to_boss};
-			
-			# Send the initial request
-			$self->send($to_boss, {pid => $$});
-			
-			# Start processing
-			$self->worker($from_boss);
-			
-			# When the worker subroutine completes, exit
-			exit;
-		} else {
-			confess("Failed to fork: $!");
-		}
-	}
-	
-	# Close unused pipes
-	close($self->{to_boss});
-	delete($self->{to_boss});
+    my $self = shift();
+    
+    # Create a pipe for the workers to communicate to the boss
+    
+    # Create the workers
+    foreach (1..$self->{worker_count}) {
+        
+        # Open a pipe for the worker
+        pipe(my $from_boss, my $to_worker);
+        pipe(my $from_worker, my $to_boss);
+        
+        # Fork off a worker
+        my $pid = fork();
+        
+        if ($pid > 0) {
+            
+            # Boss
+            $self->{workers}->{$pid} = $to_worker;
+            $self->{from_worker}->{$pid} = $from_worker;
+            $self->{select}->add($from_worker);
+
+            # Close unused pipes
+            close($to_boss);
+            close($from_boss);
+            
+        } elsif ($pid == 0) {
+            
+            # Worker
+            
+            # Close unused pipes
+            close($from_worker);
+            close($to_worker);
+            
+            # Setup communication pipes
+            $self->{to_boss} = $to_boss;
+            open(STDIN, '/dev/null');
+            
+            # Send the initial request
+            $self->send($to_boss, {pid => $$});
+            
+            # Start processing
+            $self->worker($from_boss);
+            
+            # When the worker subroutine completes, exit
+            exit;
+        } else {
+            confess("Failed to fork: $!");
+        }
+    }
 }
 
 sub worker(\*) {
-	my $self = shift();
-	my $from_boss = shift();
-	
-	# Read instructions from the server
-	while (my $instructions = $self->receive($from_boss)) {
-		
-		# If the handler's children die, that's not our business
-		$SIG{CHLD} = 'IGNORE';
-		
-		# Execute the handler with the given instructions
-		my $result;
-		eval {
-			# Handle alarms
-			local $SIG{ALRM} = sub {
-				die "Work handler timed out."
-			};
-			
-			# Set alarm
-			alarm($self->{global_timeout});
-			
-			# Execute the handler and get it's result
-			if (defined $self->{work_handler}) {
-				$result = &{ $self->{work_handler} }($instructions);
-			}
-			
-			# Disable alarm
-			alarm(0);
-		};
-		
-		# Warn on errors
-		if ($@) {
-			croak("Worker $$ error: $@");
-		}
-		
-		# Send the result to the server
-		$self->send($self->{to_boss}, {pid => $$, data => $result});
-	}
+    my $self = shift();
+    my $from_boss = shift();
+    
+    # Read instructions from the server
+    while (my $instructions = $self->receive($from_boss)) {
+        
+        # If the handler's children die, that's not our business
+        $SIG{CHLD} = 'IGNORE';
+        
+        # Execute the handler with the given instructions
+        my $result;
+        eval {
+            # Handle alarms
+            local $SIG{ALRM} = sub {
+                die "Work handler timed out."
+            };
+            
+            # Set alarm
+            alarm($self->{global_timeout});
+            
+            # Execute the handler and get it's result
+            if (defined $self->{work_handler}) {
+                $result = &{ $self->{work_handler} }($instructions);
+            }
+            
+            # Disable alarm
+            alarm(0);
+        };
+        
+        # Warn on errors
+        if ($@) {
+            croak("Worker $$ error: $@");
+        }
+        
+        # Send the result to the server
+        $self->send($self->{to_boss}, {pid => $$, data => $result});
+    }
 }
 
 sub receive(\*) {
-	my $self = shift();
+    my $self = shift();
 
-	# Get the file handle
-	my $fh = shift();
-	
-	# Get a value from the file handle
-	local $/ = $self->{msg_delimiter};
-	my $value = <$fh>;
-	
-	# Deserialize the data
-	no strict;
-	no warnings;
-	my $data = eval($value);
+    # Get the file handle
+    my $fh = shift();
+    
+    # Get a value from the file handle
+    local $/ = $self->{msg_delimiter};
+    my $value = <$fh>;
+    
+    # Deserialize the data
+    no strict;
+    no warnings;
+    my $data = eval($value);
 
-	if ($@) {
-		confess("Failed to deserialize data: $@");
-	}
+    if ($@) {
+        confess("Failed to deserialize data: $@");
+    }
 
-	return $data;
+    return $data;
 }
 
 sub send(\*$) {
-	my $self = shift();
+    my $self = shift();
 
-	# Get the file handle
-	my $fh = shift();
+    # Get the file handle
+    my $fh = shift();
 
-	# Get the value which will be sent
-	my $value = shift();
+    # Get the value which will be sent
+    my $value = shift();
 
-	# Print the value to the file handle
-	local $Data::Dumper::Deepcopy = 1;
-	local $Data::Dumper::Indent = 0;
-	local $Data::Dumper::Purity = 1;
-	print $fh Dumper($value) . $self->{msg_delimiter};
-	
-	# Force the file handle to flush
-	$fh->flush();
+    # Print the value to the file handle
+    local $Data::Dumper::Deepcopy = 1;
+    local $Data::Dumper::Indent = 0;
+    local $Data::Dumper::Purity = 1;
+    print $fh Dumper($value) . $self->{msg_delimiter};
+    
+    # Force the file handle to flush
+    $fh->flush();
 }
 
 1;
@@ -230,37 +248,37 @@ Parallel::Fork::BossWorker - Perl extension for easiliy creating forking queue p
 The minimal usage of Parallel::Fork::BossWorker requires you supply
 the work_handler argument which returns a hash reference.
 
-	use Parallel::Fork::BossWorker;
-	
-	# Create new BossWorker instance
-	my $bw = new Parallel::Fork::BossWorker(
-		work_handler => sub {
-				my $work = shift;
-				... do work here ...
-				return {};
-			}
-	);
-	
-	$bw->add_work({key=>"value"});
-	$bw->process();
+    use Parallel::Fork::BossWorker;
+    
+    # Create new BossWorker instance
+    my $bw = Parallel::Fork::BossWorker->new(
+        work_handler => sub {
+            my $work = shift;
+            ... do work here ...
+            return {};
+        }
+    );
+    
+    $bw->add_work({key=>"value"});
+    $bw->process();
 
 Additionally, you could specify the result_handler argument, which
 is passed the hash reference returned from your work_handler.
 
-	use Parallel::Fork::BossWorker;
-	
-	# Create new BossWorker instance
-	my $bw = new Parallel::Fork::BossWorker(
-		work_handler => sub {
-				my $work = shift;
-				... do work here ...
-				return {result => "Looks good"};
-			},
-		result_handler => sub {
-			my $result = shift;
-			print "$result->{result}\n";
-		}
-	);
+    use Parallel::Fork::BossWorker;
+    
+    # Create new BossWorker instance
+    my $bw = Parallel::Fork::BossWorker->new(
+        work_handler => sub {
+            my $work = shift;
+            ... do work here ...
+            return {result => "Looks good"};
+        },
+        result_handler => sub {
+            my $result = shift;
+            print "$result->{result}\n";
+        }
+    );
 
 =head1 DESCRIPTION
 
@@ -280,7 +298,7 @@ This process repeats until the work queue is empty.
 
 Creates and returns a new Parallel::Fork::BossWorker object.
 
-	my $bw = Parallel::Fork::BossWorker->new(work_handler => \&routine)
+    my $bw = Parallel::Fork::BossWorker->new(work_handler => \&routine)
 
 Parallel::Fork::BossWorker has options which allow you to customize
 how exactly the queue is handled and what is done with the data.
@@ -328,13 +346,13 @@ not appear in your data.
 
 Adds work to the instance's queue.
 
-	$bw->add_work({data => "my data"});
+    $bw->add_work({data => "my data"});
 
 =head2 process()
 
 Forks off the child processes and begins processing data.
 
-	$bw->process();
+    $bw->process();
 
 =head1 REQUIREMENTS
 
@@ -346,9 +364,11 @@ Data::Dumper
 
 IO::Handle
 
+IO::Select
+
 =head1 BUGS
 
-None I'm aware of yet, but I'm sure I'll receive reports :)
+If we knew about any bugs, we would have fixed them. :)
 
 =head1 SEE ALSO
 
@@ -356,35 +376,18 @@ None I'm aware of yet, but I'm sure I'll receive reports :)
 
 Jeff Rodriguez, E<lt>jeff@jeffrodriguez.comE<gt>
 
+Tim Wilde, E<lt>twilde@cpan.orgE<gt>
+
 =head1 COPYRIGHT AND LICENSE
 
 Copyright (c) 2007, Jeff Rodriguez
 
+Portions Copyright (c) 2011, Tim Wilde
+
 All rights reserved.
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-   * Redistributions of source code must retain the above copyright notice,
-     this list of conditions and the following disclaimer.
-
-   * Redistributions in binary form must reproduce the above copyright notice,
-     this list of conditions and the following disclaimer in the documentation
-     and/or other materials provided with the distribution.
-
-   * Neither the name of the <ORGANIZATION> nor the names of its contributors
-     may be used to endorse or promote products derived from this software
-     without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
-ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8.8 or,
+at your option, any later version of Perl 5 you may have available.
 
 =cut
